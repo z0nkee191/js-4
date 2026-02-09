33536e3a777eef2573fb7bc00d7f6461afd0981f
z0nkee191 <199979286+z0nkee191@users.noreply.github.com>
2026-02-09 18:18:33 +0200

    dz

--- js/main.js
+++ js/main.js
@@ -8,34 +8,34 @@ if (inputUser1 && inputUser2) {
     console.log("Не всі поля заповнені");
 }
 
-// const inputNum1 = Number(prompt("enter num"));
-// const inputNum2 = Number(prompt("enter num"));
-// const result = inputNum1 + inputNum2;
-// if (result > 10) {
-//     console.log("Сума більша за 10");
-// } else {
-//     console.log("Сума менша або дорівнює 10");
-// }
+const inputNum1 = Number(prompt("enter num"));
+const inputNum2 = Number(prompt("enter num"));
+const result = inputNum1 + inputNum2;
+if (result > 10) {
+    console.log("Сума більша за 10");
+} else {
+    console.log("Сума менша або дорівнює 10");
+}
 
-// const inputStr = prompt("enter str").toLowerCase().trim();
-// if (inputStr.includes("javascript")) {
-//     console.log("Текст містить слово JavaScript");
-// } else {
-//     console.log("Текст не містить слово JavaScript");
-// }
+const inputStr = prompt("enter str").toLowerCase().trim();
+if (inputStr.includes("javascript")) {
+    console.log("Текст містить слово JavaScript");
+} else {
+    console.log("Текст не містить слово JavaScript");
+}
 
-// const inputNum3 = Number(prompt("enter num"));
-// if (inputNum3 > 10 && inputNum3 < 20) {
-//     console.log("Число входить в діапазон від 10 до 20");
-// } else { 
-//     console.log("Число не входить в діапазон від 10 до 20");
-// }
+const inputNum3 = Number(prompt("enter num"));
+if (inputNum3 > 10 && inputNum3 < 20) {
+    console.log("Число входить в діапазон від 10 до 20");
+} else { 
+    console.log("Число не входить в діапазон від 10 до 20");
+}
 
-// const userName = prompt("enter your name");
-// const userEmail = prompt("enter you e-mail");
-// const userPassword = prompt("enter your password");
-// if (userName.length >= 3 && userEmail.includes("@") && userEmail.includes(".") && userPassword.length >= 6) {
-//     alert("Перенаправлення на іншу сторінку");
-// } else {
-//     alert("Помилка: неправильне заповнення");
-// }   
+const userName = prompt("enter your name");
+const userEmail = prompt("enter you e-mail");
+const userPassword = prompt("enter your password");
+if (userName.length >= 3 && userEmail.includes("@") && userEmail.includes(".") && userPassword.length >= 6) {
+    alert("Перенаправлення на іншу сторінку");
+} else {
+    alert("Помилка: неправильне заповнення");
+}   
